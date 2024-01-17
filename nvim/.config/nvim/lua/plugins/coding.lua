515e805328f7d6164e96d3a200e9bfe5653cb16f
Luciano Piñol <luchemma@gmail.com>
2024-01-17 03:27:32 -0300

    add markdown preview plugin

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -28,4 +28,14 @@ return {
 			})
 		end,
 	},
+
+	-- markdown preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
