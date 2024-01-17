274acef7c82454c8291cff504532d50a180b024a
Luciano Piñol <luchemma@gmail.com>
2024-01-17 18:37:01 -0300

    add vscode theme on colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,55 +1,59 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    opts = {
-      transparent_background = true,
-      flavour = "mocha",
-    },
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-      notify = false,
-      mini = {
-        enabled = true,
-        indentscope_color = "",
-      },
-      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
-  },
-  {
-    "miikanissi/modus-themes.nvim",
-    name = "modus",
-    priority = 1000,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    priority = 1000,
-  },
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "morhetz/gruvbox",
-    priority = 1000,
-  },
-  {
-    "xiyaowong/transparent.nvim",
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa",
-    },
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		opts = {
+			transparent_background = true,
+			flavour = "mocha",
+		},
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			nvimtree = true,
+			treesitter = true,
+			notify = false,
+			mini = {
+				enabled = true,
+				indentscope_color = "",
+			},
+			-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+		},
+	},
+	{
+		"miikanissi/modus-themes.nvim",
+		name = "modus",
+		priority = 1000,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		priority = 1000,
+	},
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"morhetz/gruvbox",
+		priority = 1000,
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		priority = 1000,
+	},
+	{
+		"xiyaowong/transparent.nvim",
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "kanagawa",
+		},
+	},
 }
