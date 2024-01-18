e9d16c593a8c068ea1953cb8e0367cf583d0346d
Luciano Piñol <luchemma@gmail.com>
2024-01-18 14:47:15 -0300

    add formatter & lint config

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -32,4 +32,41 @@ return {
 			require("package-info").setup()
 		end,
 	},
+
+	-- formatter
+	{
+		"stevearc/conform.nvim",
+		optional = true,
+		opts = {
+			formatters_by_ft = {
+				javascript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				css = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
+				yaml = { { "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+				graphql = { { "prettierd", "prettier" } },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+		},
+	},
+
+	--  linter
+	{
+		"mfussenegger/nvim-lint",
+		opts = {
+			linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+				python = { "pylint" },
+				markdown = { "markdownlint" },
+			},
+		},
+	},
 }
