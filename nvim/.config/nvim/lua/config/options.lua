d674d692c58c364f16292fca43a9610cc4824c89
Luciano Piñol <luchemma@gmail.com>
2024-01-17 23:35:57 -0300

    change indent size & font to terminator

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.wrap = true
-vim.opt.mouse = ""
+local opt = vim.opt
+
+opt.wrap = true
+opt.mouse = ""
+opt.shiftwidth = 4
+opt.tabstop = 4
