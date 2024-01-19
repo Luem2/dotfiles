5be97f419e0a08d1a6f3246186192e5c78ec4ad9
Luciano Piñol <luchemma@gmail.com>
2024-01-19 20:47:16 -0300

    add shortcut font reset & add ollama gen plugin

---
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -0,0 +1,8 @@
+--Generate text using LLMs with customizables promts (ollama)
+
+-- Minimal configuration
+return {
+	"David-Kunz/gen.nvim",
+	-- docker container running ollama
+	container = "mistral",
+}
