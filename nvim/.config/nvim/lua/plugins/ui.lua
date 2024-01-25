d2ec4ad331a3fe9687d99cd7aea0888982dbb06a
Luciano Piñol <luchemma@gmail.com>
2024-01-24 21:44:12 -0300

    config neo-tree
    right placement, show dotfiles & gitignored, close neotree explorer when open a file

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -36,4 +36,38 @@ return {
 			require("colorizer").setup()
 		end,
 	},
+
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		opts = {
+			close_if_last_window = true,
+			window = {
+				position = "right",
+			},
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						--auto close
+						-- vimc.cmd("Neotree close")
+						-- or
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		},
+	},
 }
