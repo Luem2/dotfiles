7e67a85dd5794f181243799a1ecbd90274bb3175
Luciano Piñol <luchemma@gmail.com>
2024-02-04 22:54:13 -0300

    change theme and delete unnecessary & unused code
    telescope commands (unused) & catppuccin custom config (unnecesary)

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,23 +5,6 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		lazy = false,
-		opts = {
-			transparent_background = true,
-			flavour = "mocha",
-		},
-		integrations = {
-			cmp = true,
-			gitsigns = true,
-			nvimtree = true,
-			treesitter = true,
-			notify = false,
-			mini = {
-				enabled = true,
-				indentscope_color = "",
-			},
-			-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-		},
 	},
 	{
 		"miikanissi/modus-themes.nvim",
@@ -53,7 +36,7 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "kanagawa",
+			colorscheme = "tokyonight",
 		},
 	},
 }
