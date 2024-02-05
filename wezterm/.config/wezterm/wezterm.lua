05d59b76694e2021ac2f20c69cd263e204db28b4
Luciano Piñol <luchemma@gmail.com>
2024-02-04 22:52:08 -0300

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+return {
+	-- settings
+	window_background_opacity = 0.96,
+	color_scheme = "tokyonight",
+	enable_tab_bar = false,
+	font = wezterm.font({
+		family = "CaskaydiaCove Nerd Font",
+		scale = 1.2,
+	}),
+	font_size = 12,
+	default_prog = { "zsh", "-c", "tmux attach || tmux new-session" },
+}
