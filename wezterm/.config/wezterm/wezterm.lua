341e918428a4a89c4eb3231d09e032bbfc864188
Luciano Piñol <lucianoepinol@gmail.com>
2024-02-06 01:02:03 -0300

    fix wsl tmux & add multiplatform config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,19 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
+local os_name = os.getenv("OS")
 
-return {
-	-- settings
-	window_background_opacity = 0.96,
-	color_scheme = "tokyonight",
-	enable_tab_bar = false,
-	font = wezterm.font({
-		family = "CaskaydiaCove Nerd Font",
-		scale = 1.2,
-	}),
-	font_size = 12,
+local config = {
+    window_background_opacity = 0.96,
+    color_scheme = "tokyonight",
+    enable_tab_bar = false,
+    font = wezterm.font({
+        family = "CaskaydiaCove Nerd Font",
+        scale = 1.2,
+    }),
+    font_size = 12,
 }
+
+if os_name and type(os_name) == 'string' and string.find(os_name:lower(), 'windows') then
+    config.default_domain = 'WSL:Ubuntu'
+end
+
+return config
