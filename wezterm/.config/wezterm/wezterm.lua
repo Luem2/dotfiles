c1eff7a7324fd432df7b2aafac5fd0684b2a3a36
Luciano Piñol <luchemma@gmail.com>
2024-02-12 21:44:56 -0300

    update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,30 +1,167 @@
 local wezterm = require("wezterm")
-local os_name = os.getenv("OS")
+local config = {}
 
-local config = {
-	window_background_opacity = 0.96,
-	disable_default_key_bindings = true,
-	color_scheme = "tokyonight",
-	enable_tab_bar = false,
-	font = wezterm.font({
-		family = "CaskaydiaCove Nerd Font",
-		scale = 1.2,
-	}),
-	font_size = 12,
-	window_decorations = "RESIZE",
+-- Use config builder object if possible
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
 
-	-- keys assignments
-	keys = {
-		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
-		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
-		{ key = "+", mods = "CTRL|SHIFT", action = wezterm.action.IncreaseFontSize },
-		{ key = "_", mods = "CTRL|SHIFT", action = wezterm.action.DecreaseFontSize },
-		{ key = ")", mods = "CTRL|SHIFT", action = wezterm.action.ResetFontSize },
+-- Settings
+config.default_prog = { "/usr/bin/zsh", "-l" }
+
+config.window_background_opacity = 0.9
+config.color_scheme = "tokyonight"
+config.font = wezterm.font({
+	family = "CaskaydiaCove Nerd Font",
+	scale = 1.1,
+})
+config.font_size = 12
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.default_workspace = "main"
+config.use_fancy_tab_bar = false
+
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+	saturation = 0.24,
+	brightness = 0.5,
+}
+
+-- keys
+config.leader = { key = "w", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+
+	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+
+	{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	{ key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+	{ key = "o", mods = "LEADER", action = wezterm.action.RotatePanes("Clockwise") },
+
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+	},
+
+	{ key = "t", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "]", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "n", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
+
+	{
+		key = "e",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Renaming Tab Title...:" },
+			}),
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+
+	-- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+	{ key = "{", mods = "LEADER|SHIFT", action = wezterm.action.MoveTabRelative(-1) },
+	{ key = "}", mods = "LEADER|SHIFT", action = wezterm.action.MoveTabRelative(1) },
+
+	-- Lastly, workspace
+	{ key = "w", mods = "LEADER", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+}
+
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
 	},
 }
 
-if os_name and type(os_name) == "string" and string.find(os_name:lower(), "windows") then
-	config.default_domain = "WSL:Ubuntu"
+-- tab customization
+local function tab_title(tab_info)
+	local title = tab_info.tab_title
+
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
 end
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+wezterm.on("format-tab-title", function(tab, _, _, _, hover, max_width)
+	local edge_background = "#0b0022"
+	local background = "#1b1032"
+	local foreground = "#808080"
+
+	if tab.is_active then
+		background = "#2b2042"
+		foreground = "#c0c0c0"
+	elseif hover then
+		background = "#3b3052"
+		foreground = "#909090"
+	end
+
+	local edge_foreground = background
+	local title = tostring(tab.tab_index + 1) .. " " .. tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	title = wezterm.truncate_right(title, max_width - 2)
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
 
 return config
