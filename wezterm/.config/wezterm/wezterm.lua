f420fef40cfbed5c6f003e7f4ec707a5878bfefd
Luciano Piñol <luchemma@gmail.com>
2024-02-13 22:43:11 -0300

    delete tab customization wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require("wezterm")
 local config = {}
 
--- Use config builder object if possible
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
@@ -110,58 +109,5 @@ config.key_tables = {
 		{ key = "Enter", action = "PopKeyTable" },
 	},
 }
-
--- tab customization
-local function tab_title(tab_info)
-	local title = tab_info.tab_title
-
-	-- if the tab title is explicitly set, take that
-	if title and #title > 0 then
-		return title
-	end
-
-	-- Otherwise, use the title from the active pane
-	-- in that tab
-	return tab_info.active_pane.title
-end
-
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-
--- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
-wezterm.on("format-tab-title", function(tab, _, _, _, hover, max_width)
-	local edge_background = "#0b0022"
-	local background = "#1b1032"
-	local foreground = "#808080"
-
-	if tab.is_active then
-		background = "#2b2042"
-		foreground = "#c0c0c0"
-	elseif hover then
-		background = "#3b3052"
-		foreground = "#909090"
-	end
-
-	local edge_foreground = background
-	local title = tostring(tab.tab_index + 1) .. " " .. tab_title(tab)
-
-	-- ensure that the titles fit in the available space,
-	-- and that we have room for the edges.
-	title = wezterm.truncate_right(title, max_width - 2)
-
-	return {
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_LEFT_ARROW },
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_RIGHT_ARROW },
-	}
-end)
 
 return config
