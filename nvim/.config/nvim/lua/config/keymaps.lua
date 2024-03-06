aa0ea9f85a6507faa5dd8d7fbc529bb677720ce2
Luciano Piñol <luchemma@gmail.com>
2024-03-06 17:04:19 -0300

    vscode - neovim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,13 +8,24 @@ local opts = { noremap = true, silent = true }
 -- select all
 keymap.set("n", "<leader>a", "gg<S-v>G", opts)
 
--- smooth motions
-keymap.set("n", "<C-d>", "<C-d>zz")
-keymap.set("n", "<C-u>", "<C-u>zz")
-
 -- increment/decrement number
 keymap.set("n", "<leader>+", "<C-a>", opts)
 keymap.set("n", "<leader>-", "<C-x>", opts)
 
--- oil.nvim
-keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+-- lazyvim specific
+if not vim.g.vscode then
+	-- smooth motions
+	keymap.set("n", "<C-d>", "<C-d>zz")
+	keymap.set("n", "<C-u>", "<C-u>zz")
+
+	-- oil.nvim
+	keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+
+	-- vscode specific
+else
+	-- disable integrated terminal
+	keymap.set({ "n", "v" }, "<C-/>", "<Nop>")
+
+	-- disable lazy menu
+	keymap.set({ "n", "v" }, "<leader>l", "<Nop>")
+end
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,7 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- neovide settings
 if vim.g.neovide then
 	vim.o.guifont = "CaskaydiaCove Nerd Font:h14"
 	vim.g.neovide_transparency = 0.80
+end
+
+-- vscode settings
+if vim.g.vscode then
+	local vscode = require("vscode-neovim")
+
+	vim.notify = vscode.notify
 end
