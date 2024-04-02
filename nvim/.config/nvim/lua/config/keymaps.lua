943e451af6d4a2b0af55641b74256b79b200f5b2
Luciano Piñol <luchemma@gmail.com>
2024-04-02 16:40:53 -0300

    split vscode from neovim config, change alacritty font

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,24 +8,20 @@ local opts = { noremap = true, silent = true }
 -- select all
 keymap.set("n", "<leader>a", "gg<S-v>G", opts)
 
+-- fast navigation between the first 5 buffers
+keymap.set("n", "<leader>1", "<cmd>BufferLineGoToBuffer 1<CR>", opts)
+keymap.set("n", "<leader>2", "<cmd>BufferLineGoToBuffer 2<CR>", opts)
+keymap.set("n", "<leader>3", "<cmd>BufferLineGoToBuffer 3<CR>", opts)
+keymap.set("n", "<leader>4", "<cmd>BufferLineGoToBuffer 4<CR>", opts)
+keymap.set("n", "<leader>5", "<cmd>BufferLineGoToBuffer 5<CR>", opts)
+
+-- smooth motions
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- increment/decrement number
 keymap.set("n", "<leader>+", "<C-a>", opts)
 keymap.set("n", "<leader>-", "<C-x>", opts)
 
--- lazyvim specific
-if not vim.g.vscode then
-	-- smooth motions
-	keymap.set("n", "<C-d>", "<C-d>zz")
-	keymap.set("n", "<C-u>", "<C-u>zz")
-
-	-- oil.nvim
-	keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-
-	-- vscode specific
-else
-	-- disable integrated terminal
-	keymap.set({ "n", "v" }, "<C-/>", "<Nop>")
-
-	-- disable lazy menu
-	keymap.set({ "n", "v" }, "<leader>l", "<Nop>")
-end
+-- oil.nvim
+keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,15 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- neovide settings
-if vim.g.neovide then
-	vim.o.guifont = "CaskaydiaCove Nerd Font:h14"
-	vim.g.neovide_transparency = 0.80
-end
+local opt = vim.opt
 
--- vscode settings
-if vim.g.vscode then
-	local vscode = require("vscode-neovim")
+-- tab & indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
 
-	vim.notify = vscode.notify
-end
+-- line wrapping
+opt.wrap = true
+
+-- exrc (local .nvimrc config)
+opt.exrc = true
