6d2ac38c67e050d869fdf0000e5c475c0fb06bb4
luem2 <luchemma@gmail.com>
2024-11-07 19:37:26 -0300

    delete codeium plugin & update .zshrc

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -13,26 +13,4 @@ return {
 	{
 		"rafamadriz/friendly-snippets",
 	},
-
-	-- codeium
-	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-		config = function()
-			-- disable default keymaps from command line
-			vim.g.codeium_disable_bindings = 1
-
-			vim.keymap.set("i", "<C-l>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true, silent = true })
-
-			vim.keymap.set("i", "<C-Z>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true, silent = true })
-
-			vim.keymap.set("i", "<C-X>", function()
-				return vim.fn["codeium#Complete"]()
-			end, { expr = true, silent = true })
-		end,
-	},
 }
